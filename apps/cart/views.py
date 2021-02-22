2d270c5a92bdb7649071ad012f37f03671bb78d4
AhmedAbdel-Moniem <ahmed.moname@gmail.com>
2021-02-22 13:03:18 +0200

    fixing the cart link

--- apps/cart/views.py
+++ apps/cart/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
 
-
 def cart(request):
     return render(request, 'cart.html')
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -2,18 +2,19 @@ from django.contrib import admin
 from django.urls import path
 from apps.core import views as coreviews
 from apps.store import views as storeviews
-from apps.cart.views import cart
+from apps.cart import views as cartviews
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     
     path('', coreviews.frontend, name='frontpage'),
+    path('cart/', cartviews.cart, name='cart'),
     path('contact/', coreviews.contact, name='contact'),
     path('about/', coreviews.about, name='about'),
     
     path('<slug:category_slug>/<slug:slug>/', storeviews.product_details, name='product_detail'),
     path('<slug:slug>/', storeviews.category_detail, name='category_detail'),
     
-    path('cart/', cart, name='cart'),
+    
 ]
